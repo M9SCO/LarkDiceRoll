eac4b1f06912338b32286d303242e38440862c55
M9SCO <chr.grigorii@gmail.com>
2022-11-09 23:16:06 +0600

    updating versions

--- rolling_dice/__init__.py
+++ rolling_dice/__init__.py
@@ -1,7 +1,7 @@
 import sys
 
 if sys.version_info < (3, 7):
-    raise ImportError('Your Python version {0} is not supported by aiogram, please install '
+    raise ImportError('Your Python version {0} is not supported by dice_roll, please install '
                       'Python 3.7+'.format('.'.join(map(str, sys.version_info[:3]))))
 
 from rolling_dice.models import Dice, Result, DiceError, NotFoundMethod, ParseError
@@ -20,4 +20,4 @@ __all__ = (
     "GRAMMAR_DICE"
 )
 
-__version__ = '1.1'
+__version__ = '1.2'
